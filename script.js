5f291e2abc19a9dd15f3283380b27fe928d2e9b2
negin-mgdm <negin.mgdm@gmail.com>
2024-09-07 17:54:27 +0100

    feat: Adding array of fortune messages.

---
+++ script.js
@@ -0,0 +1,11 @@
+// Array of fortune messages
+const fortunes = [
+    "You will have a pleasant surprise.",
+    "A thrilling time is in your near future.",
+    "Your talents will be recognized and rewarded.",
+    "Now is the time to try something new.",
+    "An unexpected event will soon bring you fortune.",
+    "You will find what you seek where you least expect it.",
+    "A new perspective will come with the new year.",
+    "Someone is looking up to you. Don't let them down."
+];
